a864dc014b0def1c90e9b9104ec5d2b56e7e41e8
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-05-10 14:26:41 +0800

    Add AIP command to package.json and extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "click-commit" is now active!');
 
-	let disposable = vscode.commands.registerCommand('extension.runAIC', async () => {
-		const currentWorkspace = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
-		if (!currentWorkspace) {
-			vscode.window.showErrorMessage('No workspace folder open');
-			return;
-		}
-	
-		const terminal = vscode.window.createTerminal('aic');
-		terminal.show();
-		terminal.sendText('aic');
+	const runAICCommand = vscode.commands.registerCommand('extension.runAIC', async () => {
+		runCommandInTerminal('aic');
 	});
-	
-	context.subscriptions.push(disposable);
-	
+
+	const runAIPCommand = vscode.commands.registerCommand('extension.runAIP', async () => {
+		runCommandInTerminal('aip');
+	});
+
+	context.subscriptions.push(runAICCommand);
+	context.subscriptions.push(runAIPCommand);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
+
+function runCommandInTerminal(command: string) {
+	const currentWorkspace = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
+	if (!currentWorkspace) {
+		vscode.window.showErrorMessage('No workspace folder open');
+		return;
+	}
+
+	const terminal = vscode.window.createTerminal(command);
+	terminal.show();
+	terminal.sendText(command);
+}
